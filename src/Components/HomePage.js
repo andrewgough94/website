20cc5b9e46179f1ec0a99c825f1ca8907cf7978c
Andrew Gough <andrewgough94@gmail.com>
2019-02-18 13:12:50 -0800

    Added buttons for my social links

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -16,6 +16,10 @@ const HomeDiv = styled.div`
     background: linear-gradient(20deg, rgb(219, 112, 147), rgb(218, 163, 87));
 `;
 
+const ContentDiv = styled.div`
+    background: lightgray;
+`;
+
 const Button = styled.a`
   display: inline-block;
   border-radius: 3px;
@@ -27,13 +31,34 @@ const Button = styled.a`
   border: 2px solid white;
 `;
 
+const Header = styled.h2`
+   background: lightblue;
+`
+
 const HomePage = () => {
     return (
         <HomeDiv>
-            <div> Hey this is a homepage </div>
-            <Button
-                href="https://github.com/andrewgough94/"
-            > I'm a button </Button>
+            <ContentDiv>
+                <Header> This is my header </Header>
+                <div> Here are links to my tings </div>
+                <Button
+                    href="https://github.com/andrewgough94/"
+                    rel="noopener"
+                    target="_blank"
+                > Github </Button>
+                <Button
+                    href="https://www.linkedin.com/in/andrew-gough-9b130771"
+                    rel="noopener"
+                    target="_blank"
+                > LinkedIn </Button>
+                <Button
+                    href="https://medium.com/@andrewgough94/"
+                    rel="noopener"
+                    target="_blank"
+                > Medium </Button>
+
+
+            </ContentDiv>
         </HomeDiv>
     );
 };
